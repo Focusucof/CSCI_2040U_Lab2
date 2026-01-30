d4b02eb5314a2caa5eb2157fa0c588db3e967ea5
Ashwins1971 <144913868+Ashwins1971@users.noreply.github.com>
2026-01-30 15:25:45 -0500

    frontend.py
    CatalogMenu

---
+++ frontend.py
@@ -0,0 +1,33 @@
+def CatalogMenu():
+    print("1: View items")
+    print("2: Add item")
+    print("3: Edit item")
+    print("4: Exit")
+
+
+def main():
+    while True:
+        menu()
+        choice = input("Choose: ")
+
+        if choice == "1":
+            print("Viewing items")
+            # back end 
+
+        elif choice == "2":
+            name = input("Enter name: ")
+            # back end 
+            print("Item added")
+
+        elif choice == "3":
+            # back end
+            print("Item edited")
+
+        elif choice == "4":
+            break
+
+        else:
+            print("Invalid option")
+
+
+main()
